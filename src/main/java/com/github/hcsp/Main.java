312969a65acdbe24e6a35819fdd299efc64c0596
pathjh <1639925487@qq.com>
2020-03-24 11:05:22 +0800

    加入基本功能并重构代码 (#3)
    * 加入基本功能并重构代码

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -6,18 +6,81 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //待处理链接池
+        List<String> linkPool = new ArrayList<>();
+        //已处理链接池
+        Set<String> processedLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            String link = linkPool.remove(linkPool.size() - 1);
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+            if (isInternetLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                ArrayList<Element> articalTags = doc.select("article");
+                storeIntoDatabaseIfItIsNewsPage(articalTags);
+                processedLinks.add(link);
+            }
+        }
+    }
+
+    private static void storeIntoDatabaseIfItIsNewsPage(ArrayList<Element> articalTags) {
+        if (!articalTags.isEmpty()) {
+            for (Element articalTag : articalTags
+            ) {
+                String title = articalTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn");
+        System.out.println(link);
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("user-agent", " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36");
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
             System.out.println(response1.getStatusLine());
             HttpEntity entity1 = response1.getEntity();
-            System.out.println(EntityUtils.toString(entity1));
-            //增加注释测试CIs是否正常
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
         }
     }
+
+    private static boolean isInternetLink(String link) {
+        return isNotLoginPage(link) && (isNewsPageLink(link) || isIndexPage(link)) && isNormalLink(link);
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
+    }
+
+    private static boolean isNewsPageLink(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNormalLink(String link) {
+        return !link.contains("\\/");
+    }
+
 }
